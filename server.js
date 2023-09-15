bda3f3cfd8ecce58dbd32d04d8988cbc151feeca
Axel Tokam <mail.axeltokam@gmail.com>
2023-09-15 16:20:15 +0100

    Edited views in server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // View_Engine
 app.set("view engine", "ejs");
-app.use("/assets", express.static("./static"));
+app.use("/assets", express.static(path.join(__dirname, "static"))); 
 
 // Middlewares 
 app.use(express.urlencoded({ extended: false }));
