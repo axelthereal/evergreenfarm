9d37cec5b467af40d1dcc1babb7d6a007fac64a6
Axel Tokam <mail.axeltokam@gmail.com>
2023-09-20 23:45:25 +0100

    added discover view and about views

--- routes/pages.js
+++ routes/pages.js
@@ -14,6 +14,10 @@ router.get("/home", (req, res) => {
     context.viewtitle = "Home";
     res.render("Screens/HomeScreen.ejs", context); 
 });
+router.get("/discover", (req, res) => {
+      context.viewtitle = "Our Mission";
+      res.render("Screens/DiscoverScreen.ejs", context); 
+  });
 
 
 export const pagesRouter = router;
